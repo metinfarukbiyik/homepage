5f503c68e76f28d889cb9da82cb20100bec6d849
Metin Bıyık <mb.metinbiyik@gmail.com>
2023-03-29 01:05:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import {Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
-import Footer from './components/Footer'
 import About from './pages/About'
 import Main from './pages/Main'
 import Page404 from './pages/404'
 
 function App() {
   return (
-    <>
     <div className='container'>
     <Header />
         <Routes>
@@ -17,8 +15,7 @@ function App() {
         </Routes>
         
     </div>
-    <Footer />
-    </>
+
   );
 }
 
